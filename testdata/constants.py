a3ddd6d08d4498ef123ecdffa99a5aa8b4d4ce01
kanchan-gitlab <78693301+kanchan-gitlab@users.noreply.github.com>
2022-07-09 17:04:39 +0530

    Added constants

---
+++ testdata/constants.py
@@ -0,0 +1,10 @@
+from aenum import Enum
+
+
+class TestConstants(Enum):
+    DEFAULT_TIMEOUT = 10
+    SMALL_TIMEOUT = 3
+    USER_NAME = 'kanchan-gitlab'
+    USER_EMAIL = 'kanvi17@gmail com'
+    ACCESS_TOKEN = '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'
+    DEFAULT_BASE_URL = "https://api.github.com/"
